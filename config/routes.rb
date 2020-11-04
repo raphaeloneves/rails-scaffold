3ac028f772dcf8fca7879eb3b0de5da748d0ccf5
Raphael Neves <raphaeloneves@outlook.com>
2020-11-04 10:01:50 +0000

    feat: add default base routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  namespace :api do
+    namespace :v1 do
+
+    end
+  end
 end

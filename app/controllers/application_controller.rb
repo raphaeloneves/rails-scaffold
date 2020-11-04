59ffd66a45d498536dc612add3eae3b4656c3c14
Raphael Neves <raphaeloneves@outlook.com>
2020-11-04 09:59:44 +0000

    feat: default application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  include ActionController::Serialization
+  include ExceptionHandlerConcern
 end
